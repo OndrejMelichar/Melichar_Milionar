52aff1c0a0fcec50e38c9aad04bbdb6b9123195a
OndrejMelichar <42346135+OndrejMelichar@users.noreply.github.com>
2018-11-04 01:32:55 +0100

    Pokusy s binding - zatím nefunguje

---
+++ Melichar_Milionar/ObalUserControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XAMLUserControl
+{
+    /// <summary> 
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary> 
+
+    public partial class ObalUserControl : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string Pokus;
+
+        public ObalUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public string pokus
+        {
+            get { return Pokus; }
+            set { Pokus = value; }
+        }
+    }
+}
--- Melichar_Milionar/StatistikyStranka.xaml.cs
+++ Melichar_Milionar/StatistikyStranka.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XAMLUserControl;
 
 namespace Melichar_Milionar
 {
@@ -40,7 +41,8 @@ namespace Melichar_Milionar
 
             foreach(Hrac hrac in hraci)
             {
-
+                ObalUserControl obal = new ObalUserControl();
+                obal.pokus = "PES";
             }
         }
 
